3594cf99a9646412fd5d685c2ee3e2b806358e0f
Brynhild <402904972@qq.com>
2018-10-30 09:15:02 +0800

    用户审核表格

--- src/components/userAudit/index.vue
+++ src/components/userAudit/index.vue
@@ -1,17 +1,48 @@
 <template>
-    <div>用户注册审核</div>
+   <div>
+        <div>用户信息审核</div>
+        <div>
+            <p class="header">待审核信息：</p>
+        </div>
+        <UserAuditTbale></UserAuditTbale>
+        <auditWin></auditWin>
+   </div>
+
 </template>
 
 <script>
+import UserAuditTbale from "./userAuditTbale";
+import auditWin from "./auditWin";
+ import {
+        createNamespacedHelpers
+    } from 'vuex';
+    const {
+        mapState,
+        mapActions,
+        mapMutations
+    } = createNamespacedHelpers('UserAudit');
     export default {
         data() {
             return {
 
             }
+        },
+        methods: {
+            ...mapActions(['getApplications']),
+        },
+        components: {
+            UserAuditTbale,auditWin
+        },
+        created () {
+            console.log(111);
+            
+            this.getApplications({source:"account"});
         }
     }
 </script>
 
 <style scoped>
-
+.header{
+    text-align: left;;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import AdminAccount from "./modules/adminAccount";
 import UsersAccount from "./modules/usersAccount";
+import UserAudit from "./modules/userAudit";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    AdminAccount,UsersAccount
+    AdminAccount,UsersAccount,UserAudit
   },
   state: {
 
