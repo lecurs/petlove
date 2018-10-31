8bffc160cebbe1251c5cb8e2a144c2dfa635dda3
wang qin bing <821462045@qq.com>
2018-10-31 14:05:38 +0800

    修改模块化

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
-import Orders from "./modules/orders"
+import orders from "./modules/orders"
 export default new Vuex.Store({
   modules: {
-    Orders
+    orders
   },
   state: {
 
